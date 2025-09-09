dbf0b5ecbc0f4f0983b23819d184e1b2eac7e5fe
huynhkien <huynhhuukien2962@gmail.com>
2025-09-09 13:14:40 +0700

    Update file khCsThangRoute.js

--- src/routes/khCsThangRoute.js
+++ src/routes/khCsThangRoute.js
@@ -1,7 +1,8 @@
 const khCsThangControllers = require('../controllers/khCsThangController');
 const express = require('express');
 const router = express.Router();
+const {verifyAccessToken, checkUserPermission} = require('../middleware/jwt')
 
-router.route('/').get(khCsThangControllers.get_khCsThang)
+router.route('/').get([verifyAccessToken, checkUserPermission],khCsThangControllers.get_khCsThang)
 
 module.exports = router;
