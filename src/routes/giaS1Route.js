96b68bd2ffd13264756d02d031c7622ef738522a
huynhkien <huynhhuukien2962@gmail.com>
2025-09-09 13:13:53 +0700

    Update file giaS1Route.js

--- src/routes/giaS1Route.js
+++ src/routes/giaS1Route.js
@@ -1,7 +1,8 @@
 const giaS1Controllers = require('../controllers/giaS1Controller');
 const express = require('express');
 const router = express.Router();
+const {verifyAccessToken, checkUserPermission} = require('../middleware/jwt')
 
-router.route('/').get(giaS1Controllers.get_giaS1);
+router.route('/').get([verifyAccessToken, checkUserPermission],giaS1Controllers.get_giaS1);
 
 module.exports = router;
