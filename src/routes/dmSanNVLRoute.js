865d5cd59aa8825f94bfbf6a028a76492e812733
huynhkien <huynhhuukien2962@gmail.com>
2025-09-09 13:12:48 +0700

    Update file dmSanNVLRoute.js

--- src/routes/dmSanNVLRoute.js
+++ src/routes/dmSanNVLRoute.js
@@ -1,8 +1,9 @@
 const dmSanNVLControllers = require('../controllers/dmSanNVLController');
 const express = require('express');
 const router = express.Router();
+const {verifyAccessToken, checkUserPermission} = require('../middleware/jwt')
 
-router.route('/').get(dmSanNVLControllers.get_dmSanNVL);
+router.route('/').get([verifyAccessToken, checkUserPermission],dmSanNVLControllers.get_dmSanNVL);
 
 
 module.exports = router;
