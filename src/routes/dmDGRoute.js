57838975ab27bf83298132ded2dba2e60d08c12e
huynhkien <huynhhuukien2962@gmail.com>
2025-09-09 13:11:35 +0700

    Update file dmDGRoute.js

--- src/routes/dmDGRoute.js
+++ src/routes/dmDGRoute.js
@@ -1,12 +1,13 @@
 const dmDGControllers = require('../controllers/dmDGController');
 const express = require('express');
 const router = express.Router();
+const {verifyAccessToken, checkUserPermission} = require('../middleware/jwt')
 
-router.route('/hu').get(dmDGControllers.get_dmDGHu);
-router.route('/bich').get(dmDGControllers.get_dmDGBich);
-router.route('/tui').get(dmDGControllers.get_dmDGTui);
-router.route('/bao').get(dmDGControllers.get_dmDGBao);
-router.route('/bigbag').get(dmDGControllers.get_dmDGBigBag);
+router.route('/hu').get([verifyAccessToken, checkUserPermission],dmDGControllers.get_dmDGHu);
+router.route('/bich').get([verifyAccessToken, checkUserPermission],dmDGControllers.get_dmDGBich);
+router.route('/tui').get([verifyAccessToken, checkUserPermission],dmDGControllers.get_dmDGTui);
+router.route('/bao').get([verifyAccessToken, checkUserPermission],dmDGControllers.get_dmDGBao);
+router.route('/bigbag').get([verifyAccessToken, checkUserPermission],dmDGControllers.get_dmDGBigBag);
 
 
 module.exports = router;
