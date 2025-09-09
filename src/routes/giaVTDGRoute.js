0499f6360002d71b1acdd5cbf98e73ccb0d7780b
huynhkien <huynhhuukien2962@gmail.com>
2025-09-09 13:14:13 +0700

    Update file giaVTDGRoute.js

--- src/routes/giaVTDGRoute.js
+++ src/routes/giaVTDGRoute.js
@@ -1,7 +1,8 @@
 const giaVTDGControllers = require('../controllers/giaVTDGController');
 const express = require('express');
 const router = express.Router();
+const {verifyAccessToken, checkUserPermission} = require('../middleware/jwt')
 
-router.route('/').get(giaVTDGControllers.get_giaVTDG);
+router.route('/').get([verifyAccessToken, checkUserPermission],giaVTDGControllers.get_giaVTDG);
 
 module.exports = router;
