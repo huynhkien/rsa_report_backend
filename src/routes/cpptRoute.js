36963436a54179b3e450ef2f43a7b80c9c4b8b34
huynhkien <huynhhuukien2962@gmail.com>
2025-09-09 13:10:25 +0700

    Update file cpptRoute.js

--- src/routes/cpptRoute.js
+++ src/routes/cpptRoute.js
@@ -1,9 +1,10 @@
 const cpptControllers = require('../controllers/cpptController');
 const express = require('express');
 const router = express.Router();
+const {verifyAccessToken, checkUserPermission} = require('../middleware/jwt')
 
-router.route('/02').get(cpptControllers.get_cppt02);
-router.route('/s1').get(cpptControllers.get_cpptS1);
+router.route('/02').get([verifyAccessToken, checkUserPermission], cpptControllers.get_cppt02);
+router.route('/s1').get([verifyAccessToken, checkUserPermission], cpptControllers.get_cpptS1);
 
 
 module.exports = router;
