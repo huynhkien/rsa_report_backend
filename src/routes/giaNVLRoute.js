1e473af0f4a3a4e12bac87e06afd3300a895fa6d
huynhkien <huynhhuukien2962@gmail.com>
2025-09-09 13:13:32 +0700

    Update file giaNVLRoute.js

--- src/routes/giaNVLRoute.js
+++ src/routes/giaNVLRoute.js
@@ -1,7 +1,8 @@
 const giaNVLControllers = require('../controllers/giaNVLController');
 const express = require('express');
 const router = express.Router();
+const {verifyAccessToken, checkUserPermission} = require('../middleware/jwt')
 
-router.route('/').get(giaNVLControllers.get_giaNVL);
+router.route('/').get([verifyAccessToken, checkUserPermission],giaNVLControllers.get_giaNVL);
 
 module.exports = router;
