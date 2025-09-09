a82d510c19afa715f5492d04365017c0789790f0
huynhkien <huynhhuukien2962@gmail.com>
2025-09-09 13:15:11 +0700

    Update file priceVNDRoute.js

--- src/routes/priceVNDRoute.js
+++ src/routes/priceVNDRoute.js
@@ -1,7 +1,8 @@
 const priceVNDControllers = require('../controllers/priceVNDController');
 const express = require('express');
 const router = express.Router();
+const {verifyAccessToken, checkUserPermission} = require('../middleware/jwt')
 
-router.route('/').get(priceVNDControllers.get_priceVND)
+router.route('/').get([verifyAccessToken, checkUserPermission],priceVNDControllers.get_priceVND)
 
 module.exports = router;
