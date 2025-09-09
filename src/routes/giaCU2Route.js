10d418ff140865c228864b5eeb10ab42ddbc315c
huynhkien <huynhhuukien2962@gmail.com>
2025-09-09 13:13:12 +0700

    Update file giaCU2Route.js

--- src/routes/giaCU2Route.js
+++ src/routes/giaCU2Route.js
@@ -1,7 +1,8 @@
 const giaCU2Controllers = require('../controllers/giaCU2Controller');
 const express = require('express');
 const router = express.Router();
+const {verifyAccessToken, checkUserPermission} = require('../middleware/jwt')
 
-router.route('/').get(giaCU2Controllers.get_giaCU2);
+router.route('/').get([verifyAccessToken, checkUserPermission],giaCU2Controllers.get_giaCU2);
 
 module.exports = router;
