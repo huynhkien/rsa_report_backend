38a4fc9c839d0b4fbe7747e1be5ce455c882752f
huynhkien <huynhhuukien2962@gmail.com>
2025-09-09 15:55:23 +0700

    Update file dmCu2Route.js

--- src/routes/dmCU2Route.js
+++ src/routes/dmCU2Route.js
@@ -5,9 +5,11 @@ const {verifyAccessToken, checkUserPermission} = require('../middleware/jwt')
 
 router.route('/').get([verifyAccessToken, checkUserPermission],dmCU2Controllers.get_dmCU2s)
                  .post([verifyAccessToken, checkUserPermission],dmCU2Controllers.create_dmCU2);
+router.route('/delete-dmCu2s').delete([verifyAccessToken, checkUserPermission], dmCU2Controllers.delete_dmCU2s);
 
 router.route('/:id').get([verifyAccessToken, checkUserPermission],dmCU2Controllers.get_dmCU2)
                     .put([verifyAccessToken, checkUserPermission],dmCU2Controllers.edit_dmCU2)
                     .delete([verifyAccessToken, checkUserPermission],dmCU2Controllers.delete_dmCU2);
+
 
 module.exports = router;
