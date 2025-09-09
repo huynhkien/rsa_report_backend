35abda9322c54a2bf00995ab696c167c39520f00
huynhkien <huynhhuukien2962@gmail.com>
2025-09-09 13:12:15 +0700

    Update file dmRoute.js

--- src/routes/dmRoute.js
+++ src/routes/dmRoute.js
@@ -1,12 +1,13 @@
 const dmControllers = require('../controllers/dmController');
 const express = require('express');
 const router = express.Router();
+const {verifyAccessToken, checkUserPermission} = require('../middleware/jwt')
 
-router.route('/nvl').get(dmControllers.get_dsNVL);
-router.route('/vtdg').get(dmControllers.get_dsVTDG);
-router.route('/ct').get(dmControllers.get_dsCONGTHUC);
+router.route('/nvl').get([verifyAccessToken, checkUserPermission],dmControllers.get_dsNVL);
+router.route('/vtdg').get([verifyAccessToken, checkUserPermission],dmControllers.get_dsVTDG);
+router.route('/ct').get([verifyAccessToken, checkUserPermission],dmControllers.get_dsCONGTHUC);
 
-router.route('/detail-ct').get(dmControllers.get_dsCTDetail);
+router.route('/detail-ct').get([verifyAccessToken, checkUserPermission],dmControllers.get_dsCTDetail);
 
 
 module.exports = router;
