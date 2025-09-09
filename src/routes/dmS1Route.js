395426a97cca3025f2dc3c6beeb098b2fd105e74
huynhkien <huynhhuukien2962@gmail.com>
2025-09-09 14:30:50 +0700

    Update file dmS1Route.js

--- src/routes/dmS1Route.js
+++ src/routes/dmS1Route.js
@@ -3,7 +3,10 @@ const express = require('express');
 const router = express.Router();
 const {verifyAccessToken, checkUserPermission} = require('../middleware/jwt')
 
-router.route('/').get([verifyAccessToken, checkUserPermission],dmS1Controllers.get_dmS1);
-
-
+router.route('/').get([verifyAccessToken, checkUserPermission],dmS1Controllers.get_dmS1s)
+                 .post([verifyAccessToken, checkUserPermission],dmS1Controllers.create_dmS1);
+                 
+router.route('/:id').get([verifyAccessToken, checkUserPermission],dmS1Controllers.get_dmS1)
+                    .put([verifyAccessToken, checkUserPermission],dmS1Controllers.edit_dmS1)
+                    .delete([verifyAccessToken, checkUserPermission],dmS1Controllers.delete_dmS1);
 module.exports = router;
