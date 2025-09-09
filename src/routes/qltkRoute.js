a4d5eb4f1ab4d2eab5c3b640e09b6dcc61d3f700
huynhkien <huynhhuukien2962@gmail.com>
2025-09-09 13:15:32 +0700

    Update file qltkRoute.js

--- src/routes/qltkRoute.js
+++ src/routes/qltkRoute.js
@@ -1,7 +1,17 @@
 const qltkControllers = require('../controllers/qltkController');
 const express = require('express');
 const router = express.Router();
+const {verifyAccessToken, checkUserPermission} = require('../middleware/jwt')
 
-router.route('/').get(qltkControllers.get_qltk)
+// Hiển thị thông tin
+router.route('/').get(qltkControllers.get_qltk);
+// Đăng nhập
+router.route('/login').post(qltkControllers.login);
+// Đăng xuất
+router.route('/logout').get(qltkControllers.logout);
+// Tạo mới accessToken
+router.route('/newRefreshToken').get([verifyAccessToken, checkUserPermission],qltkControllers.refreshAccessToken);
+// Hiển thị thông tin đăng nhập
+router.route('/getQltkCurrent').get([verifyAccessToken, checkUserPermission], qltkControllers.getQlTkCurrent)
 
 module.exports = router;
