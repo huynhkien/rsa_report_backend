7dc2d8de4c922d47fe2a0d2130f799e312bebd85
huynhkien <huynhhuukien2962@gmail.com>
2026-03-17 09:21:09 +0700

    Update file app.js

--- app.js
+++ app.js
@@ -4,20 +4,10 @@ var cookieParser = require('cookie-parser');
 const {notFound, errorHandler} = require('./src/middleware/errHandle');
 const cors = require('cors');
 require('dotenv').config();
-const dmCU2Router = require('./src/routes/dmCU2Route');
-const dmS1Router = require('./src/routes/dmS1Route');
-const qltkRouter = require('./src/routes/qltkRoute');
-const giaCU2Router = require('./src/routes/giaCU2Route');
-const giaS1Router = require('./src/routes/giaS1Route');   
-const giaNVLRouter = require('./src/routes/giaNVLRoute');   
-const giaVTDGRouter = require('./src/routes/giaVTDGRoute');   
-const dmSanNVLRouter = require('./src/routes/dmSanNVLRoute');
-const dmDGRouter = require('./src/routes/dmDGRoute');   
-const dmRouter = require('./src/routes/dmRoute');   
-const cpptRouter = require('./src/routes/cpptRoute');   
-const dmKHRouter = require('./src/routes/dmKHRoute');    
-const khCsThangRouter = require('./src/routes/khCsThangRoute');    
-const priceVNDRouter = require('./src/routes/priceVNDRoute');    
+const PowerSupplyInfoRouter = require('./src/routes/powerSupplyInfoRoute');    
+const Coater02ReportRouter = require('./src/routes/coater02ReportRoute');    
+const CoaterS1ReportRouter = require('./src/routes/coaterS1ReportRoute');    
+const ProductionRouter = require('./src/routes/productionRoute');    
 const app = express()
 
 // JSON
@@ -43,20 +33,11 @@ app.get('/', (req, res) => {
         message: 'Server đang chạy'
     });
 });
-app.use('/api/dmCU2',dmCU2Router);
-app.use('/api/dmS1',dmS1Router);
-app.use('/api/giaCU2', giaCU2Router);
-app.use('/api/giaS1', giaS1Router);
-app.use('/api/qltk',qltkRouter);
-app.use('/api/giaNVL',giaNVLRouter);
-app.use('/api/giaVTDG',giaVTDGRouter);
-app.use('/api/dmSanNVL',dmSanNVLRouter);
-app.use('/api/dmDG',dmDGRouter);
-app.use('/api/dm',dmRouter);
-app.use('/api/cppt',cpptRouter);
-app.use('/api/dmKH', dmKHRouter);
-app.use('/api/khCsThang', khCsThangRouter);
-app.use('/api/priceVND', priceVNDRouter);
+// API 
+app.use('/api/PowerSupplyInfo', PowerSupplyInfoRouter);
+app.use('/api/Coater02', Coater02ReportRouter);
+app.use('/api/CoaterS1', CoaterS1ReportRouter);
+app.use('/api/Production', ProductionRouter);
 // Bắt các route không khớp, trả về lỗi 404
 app.use(notFound)
 // Xử lý lỗi
