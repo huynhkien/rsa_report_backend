e3606d1506795c76a215164a71e962375ecd88ef
huynhkien <huynhhuukien2962@gmail.com>
2026-03-17 09:24:12 +0700

    Delete file

--- src/routes/priceVNDRoute.js
+++
@@ -1,8 +0,0 @@
-const priceVNDControllers = require('../controllers/priceVNDController');
-const express = require('express');
-const router = express.Router();
-const {verifyAccessToken, checkUserPermission} = require('../middleware/jwt')
-
-router.route('/').get([verifyAccessToken, checkUserPermission],priceVNDControllers.get_priceVND)
-
-module.exports = router;
