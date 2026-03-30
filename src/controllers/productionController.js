32772097f4e0cb06e6bbf6668cf1ba007b4c9dc2
huynhkien <huynhhuukien2962@gmail.com>
2026-03-30 16:30:06 +0700

    Update file productionController.js

--- src/controllers/productionController.js
+++ src/controllers/productionController.js
@@ -7,8 +7,9 @@ const moment = require('moment');
 const addProduction = asyncHandler(async(req, res) => {
     // Thông tin của đợt sản phẩm
     const {DOT_SX, ME_THU, MA_TB, TG_BD, TG_KT, TRUONG_CA, 
-        NV_VH, LOAI_SP, KL_NL, KL_TP, KL_DONG, SO_LOT, TT_THAPTRANG,
-        TT_CHILLER, TT_PRESSURE_MACHINE, TT_BOILER, GHI_CHU, VT_PALLET} = req.body;
+        NV_VH, LOAI_SP, KL_NL, KL_TP, KL_DONG, SO_LOT, TT_THAPTRANG, GHI_CHU_TT_THAPTRANG,
+        TT_CHILLER, GHI_CHU_TT_CHILLER, TT_PRESSURE_MACHINE, GHI_CHU_TT_PRESSURE_MACHINE, TT_BOILER, 
+        GHI_CHU_TT_BOILER, GHI_CHU, VT_PALLET, VT_PALLET_KL_DONG, VT_PALLET_BTP} = req.body;
     // Kiểm tra thông tin của đợt sản xuất
     if(!DOT_SX || !ME_THU || !MA_TB || !TG_BD || !TG_KT || !TRUONG_CA || !NV_VH || !LOAI_SP
         || !SO_LOT 
@@ -29,20 +30,28 @@ const addProduction = asyncHandler(async(req, res) => {
         .input('KL_DONG', sql.NVarChar, KL_DONG)
         .input('SO_LOT', sql.NVarChar(50), SO_LOT)
         .input('TT_THAPTRANG', sql.NVarChar, TT_THAPTRANG)
+        .input('GHI_CHU_TT_THAPTRANG', sql.NVarChar, GHI_CHU_TT_THAPTRANG)
         .input('TT_CHILLER', sql.NVarChar, TT_CHILLER)
+        .input('GHI_CHU_TT_CHILLER', sql.NVarChar, GHI_CHU_TT_CHILLER)
         .input('TT_PRESSURE_MACHINE', sql.NVarChar, TT_PRESSURE_MACHINE)
+        .input('GHI_CHU_TT_PRESSURE_MACHINE', sql.NVarChar, GHI_CHU_TT_PRESSURE_MACHINE)
         .input('TT_BOILER', sql.NVarChar, TT_BOILER)
+        .input('GHI_CHU_TT_BOILER', sql.NVarChar, GHI_CHU_TT_BOILER)
         .input('GHI_CHU', sql.NVarChar, GHI_CHU)
         .input('VT_PALLET', sql.NVarChar, VT_PALLET)
+        .input('VT_PALLET_KL_DONG', sql.NVarChar, VT_PALLET_KL_DONG)
+        .input('VT_PALLET_BTP', sql.NVarChar, VT_PALLET_BTP)
         .query(`
             INSERT INTO DataSX_RSF 
                 (DOT_SX, ME_THU, MA_TB, TG_BD, TG_KT, TRUONG_CA, 
                     NV_VH, LOAI_SP, KL_NL, KL_TP, KL_DONG, SO_LOT, TT_THAPTRANG,
-                    TT_CHILLER, TT_PRESSURE_MACHINE, TT_BOILER, GHI_CHU, VT_PALLET)
+                    TT_CHILLER, TT_PRESSURE_MACHINE, TT_BOILER, GHI_CHU, VT_PALLET, 
+                    VT_PALLET_KL_DONG, VT_PALLET_BTP, GHI_CHU_TT_THAPTRANG, GHI_CHU_TT_CHILLER, GHI_CHU_TT_PRESSURE_MACHINE, GHI_CHU_TT_BOILER)
             VALUES
                 (@DOT_SX, @ME_THU, @MA_TB, @TG_BD, @TG_KT, @TRUONG_CA, 
                     @NV_VH, @LOAI_SP, @KL_NL, @KL_TP, @KL_DONG, @SO_LOT, @TT_THAPTRANG,
-                    @TT_CHILLER, @TT_PRESSURE_MACHINE, @TT_BOILER, @GHI_CHU, @VT_PALLET)
+                    @TT_CHILLER, @TT_PRESSURE_MACHINE, @TT_BOILER, @GHI_CHU, @VT_PALLET, @VT_PALLET_KL_DONG, @VT_PALLET_BTP,
+                     @GHI_CHU_TT_THAPTRANG, @GHI_CHU_TT_CHILLER, @GHI_CHU_TT_PRESSURE_MACHINE, @GHI_CHU_TT_BOILER)
             `);
     return res.status(200).json({
         success: true,
@@ -55,8 +64,9 @@ const updateProduction = asyncHandler(async(req, res) => {
     const {id} = req.params;
     // Thông tin của đợt sản phẩm
     const {DOT_SX, ME_THU, MA_TB, TG_BD, TG_KT, TRUONG_CA, 
-        NV_VH, LOAI_SP, KL_NL, KL_TP, KL_DONG, SO_LOT, TT_THAPTRANG,
-        TT_CHILLER, TT_PRESSURE_MACHINE, TT_BOILER, GHI_CHU, VT_PALLET} = req.body;
+        NV_VH, LOAI_SP, KL_NL, KL_TP, KL_DONG, SO_LOT, TT_THAPTRANG, GHI_CHU_TT_THAPTRANG,
+        TT_CHILLER, GHI_CHU_TT_CHILLER, TT_PRESSURE_MACHINE, GHI_CHU_TT_PRESSURE_MACHINE, TT_BOILER, 
+        GHI_CHU_TT_BOILER, GHI_CHU, VT_PALLET, VT_PALLET_KL_DONG, VT_PALLET_BTP} = req.body;
     if(!id) throw new Error('Không tìm thấy thông tin đợt sản xuất');
     // Kiểm tra thông tin của đợt sản xuất
     if(!DOT_SX || !ME_THU || !MA_TB || !TG_BD || !TG_KT || !TRUONG_CA || !NV_VH || !LOAI_SP
@@ -78,11 +88,17 @@ const updateProduction = asyncHandler(async(req, res) => {
         .input('KL_DONG', sql.NVarChar, KL_DONG)
         .input('SO_LOT', sql.NVarChar(50), SO_LOT)
         .input('TT_THAPTRANG', sql.NVarChar, TT_THAPTRANG)
+        .input('GHI_CHU_TT_THAPTRANG', sql.NVarChar, GHI_CHU_TT_THAPTRANG)
         .input('TT_CHILLER', sql.NVarChar, TT_CHILLER)
+        .input('GHI_CHU_TT_CHILLER', sql.NVarChar, GHI_CHU_TT_CHILLER)
         .input('TT_PRESSURE_MACHINE', sql.NVarChar, TT_PRESSURE_MACHINE)
+        .input('GHI_CHU_TT_PRESSURE_MACHINE', sql.NVarChar, GHI_CHU_TT_PRESSURE_MACHINE)
         .input('TT_BOILER', sql.NVarChar, TT_BOILER)
+        .input('GHI_CHU_TT_BOILER', sql.NVarChar, GHI_CHU_TT_BOILER)
         .input('GHI_CHU', sql.NVarChar, GHI_CHU)
         .input('VT_PALLET', sql.NVarChar, VT_PALLET)
+        .input('VT_PALLET_KL_DONG', sql.NVarChar, VT_PALLET_KL_DONG)
+        .input('VT_PALLET_BTP', sql.NVarChar, VT_PALLET_BTP)
         .query(`
             UPDATE DataSX_RSF 
             SET  
@@ -98,11 +114,17 @@ const updateProduction = asyncHandler(async(req, res) => {
                 KL_TP = @KL_TP,
                 KL_DONG = @KL_DONG,
                 TT_THAPTRANG = @TT_THAPTRANG,
+                GHI_CHU_TT_THAPTRANG = @GHI_CHU_TT_THAPTRANG,
                 TT_CHILLER = @TT_CHILLER,
+                GHI_CHU_TT_CHILLER = @GHI_CHU_TT_CHILLER,
                 TT_PRESSURE_MACHINE = @TT_PRESSURE_MACHINE,
+                GHI_CHU_TT_PRESSURE_MACHINE = @GHI_CHU_TT_PRESSURE_MACHINE,
                 TT_BOILER = @TT_BOILER,
+                GHI_CHU_TT_BOILER = @GHI_CHU_TT_BOILER,
                 GHI_CHU = @GHI_CHU,
                 VT_PALLET = @VT_PALLET
+                VT_PALLET_KL_DONG = @VT_PALLET_KL_DONG
+                VT_PALLET_BTP = @VT_PALLET_BTP
             WHERE SO_LOT = @SO_LOT
             `);
     return res.status(200).json({
